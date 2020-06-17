759b789d5d059cf8c22dd134034ab163d113e035
AlexeySukhanov <webdevforbiz@gmail.com>
2020-06-17 17:08:19 +0300

    ADD:  dq_find_closest_point(), dq_two_points_dist()

--- DQ functions collection/dq_functions.py
+++ DQ functions collection/dq_functions.py
@@ -30,11 +30,40 @@ def dq_list_to_freq_table(data_set, index = 0, exclude_first_row = True):
             freq_table[key] = 1         
     return freq_table
 
+# Find the distance between two points
+def dq_two_points_dist(x_1, y_1, x_2, y_2):
+    from math import sqrt
+    return sqrt((x_1 - x_2)**2 + (y_1 - y_2)**2)
+
+# Finds the closest point from the CSV list
+# dependencies: dq_csv_to_list(), dq_two_points_dist()    
+def dq_find_closest_point(x1, y1, x2_index, y2_index, csv_file_name, exclude_first_row = True):
+    coordinates_list = dq_csv_to_list(csv_file_name)
+    
+    for row in coordinates_list[1:]:
+        x2 = float(row[x2_index])
+        y2 = float(row[y2_index])
+        if 'dist' in locals():
+            current_distance = dq_two_points_dist(x1, y1, x2, y2)
+            if current_distance < distance:
+                distance = current_distance
+                result = row
+        else:
+            distance = dq_two_points_dist(x1, y1, x2, y2) 
+            result = row
+    return result
+
 
 
 # Test these functions:
-apps_data = dq_csv_to_list('AppleStore.csv')
-ratings_ft = dq_list_to_freq_table(apps_data, 7)
+#apps_data = dq_csv_to_list('AppleStore.csv')
+#ratings_ft = dq_list_to_freq_table(apps_data, 7)
 
 #print(genres_fq)
-print(ratings_ft)
+#print(ratings_ft)
+
+# provided coordinates input
+
+    
+closest_restaurant = dq_find_closest_point(100, 100, 4, 5, 'FastFoodRestaurants.csv')
+print(closest_restaurant)
